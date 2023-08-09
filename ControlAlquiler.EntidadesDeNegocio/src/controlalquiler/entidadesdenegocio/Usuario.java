6e5c57420f9af7af6c1df8c105fa3907fa751ebf
Nelson Carias <Carias030@outlook.com>
2023-08-09 11:19:00 -0600

    Programacion de la clase Usuario

--- ControlAlquiler.EntidadesDeNegocio/src/controlalquiler/entidadesdenegocio/Usuario.java
+++ ControlAlquiler.EntidadesDeNegocio/src/controlalquiler/entidadesdenegocio/Usuario.java
@@ -1,13 +1,103 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlalquiler.entidadesdenegocio;
 
-/**
- *
- * @author Henry
- */
+import java.time.LocalDate;
+
 public class Usuario {
-    
+    private int id;
+    private int idRol;
+    private String nombre;
+    private String apellido;
+    private String login;
+    private String password;
+    private byte estatus;
+    private LocalDate fechaRegistro;
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, int idRol, String nombre, String apellido, String login, String password, byte estatus, LocalDate fechaRegistro, Rol rol) {
+        this.id = id;
+        this.idRol = idRol;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.login = login;
+        this.password = password;
+        this.estatus = estatus;
+        this.fechaRegistro = fechaRegistro;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
+
