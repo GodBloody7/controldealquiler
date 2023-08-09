829cd266022e33fb493742d4cdff4ad5fc5d4f77
Henry <henryjeremyas@gmail.com>
2023-08-09 10:57:48 -0600

    Clases Rol y Categoria

---
+++ ControlAlquiler.EntidadesDeNegocio/src/controlalquiler/entidadesdenegocio/Categoria.java
@@ -0,0 +1,58 @@
+
+package controlalquiler.entidadesdenegocio;
+
+import java.util.ArrayList;
+
+public class Categoria {
+    private int id;
+    private String nombre;
+    private int top_aux;
+    private ArrayList<Equipo> equipos;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nombre, int top_aux, ArrayList<Equipo> equipos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.top_aux = top_aux;
+        this.equipos = equipos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public ArrayList<Equipo> getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(ArrayList<Equipo> equipos) {
+        this.equipos = equipos;
+    }
+    
+    
+    
+    
+    
+}
---
+++ ControlAlquiler.EntidadesDeNegocio/src/controlalquiler/entidadesdenegocio/Rol.java
@@ -0,0 +1,57 @@
+
+package controlalquiler.entidadesdenegocio;
+
+import java.util.ArrayList;
+
+public class Rol {
+    private int id;
+    private String nombre;
+    private int top_aux;
+    private ArrayList<Usuario> usuarios;
+
+    public Rol() {
+    }
+
+    public Rol(int id, String nombre, int top_aux, ArrayList<Usuario> usuarios) {
+        this.id = id;
+        this.nombre = nombre;
+        this.top_aux = top_aux;
+        this.usuarios = usuarios;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    
+    
+    
+}
